c371a5b9f8850fd8dda601bdc4ffc1043cf00cf2
CreaMira <shizipianwowanyouxi@gmail.com>
2021-04-15 17:20:33 -0700

    add 99 sample output and bug fix

--- divisors.py
+++ divisors.py
@@ -1,6 +1,10 @@
-def print_divisors(a) :
-	i = 1
-	while i <= a :
-		if (n % i==0) :
-			print i,
-       		 i = i + 1
+def printDivisors(n) :
+    i = 1
+    while i <= n :
+        if (n % i==0) :
+            print (i), 
+        i = i + 1
+         
+
+print ("The divisors of 99 are: ")
+printDivisors(99)
