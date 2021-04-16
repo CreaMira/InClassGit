212cf6364b5c691d2f2a4488e00fd9845acadf9e
CreaMira <shizipianwowanyouxi@gmail.com>
2021-04-15 17:43:00 -0700

    bug fix and print password

--- generator.py
+++ generator.py
@@ -1,10 +1,13 @@
 import random, string
 
-length = int(input(“Length of password = ”))
+length = int(input("Length of password = "))
 
-password_random_size = string.ascii_letters + string.digits + string.punctuation
+password_characters = string.ascii_letters + string.digits + string.punctuation
 
 password = []
 
 for x in range(length):
-  password.append(random.choice(password_characters))
+  password.append(random.choice(password_characters)),
+
+print ("Your password:","".join(password))
+  
