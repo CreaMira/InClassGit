a4e3060ae5af568d706cd021aa8cbd255ea0b691
CreaMira <shizipianwowanyouxi@gmail.com>
2021-04-15 17:33:31 -0700

    use for funtion to fill in a random password

--- generator.py
+++ generator.py
@@ -5,3 +5,6 @@ length = int(input(“Length of password = ”))
 password_random_size = string.ascii_letters + string.digits + string.punctuation
 
 password = []
+
+for x in range(length):
+  password.append(random.choice(password_characters))
