d87feace554a22c84abcee81afe4a5c0d01a7d4f
CreaMira <shizipianwowanyouxi@gmail.com>
2021-04-15 17:05:58 -0700

    print function and fix error

--- calc.py
+++ calc.py
@@ -1,7 +1,14 @@
-def calc(a,b);
+def calc(a,b):
     sum = a + b
     sub = a - b
     mul = a * b
     div = a / b
     mylist = [sum, sub, mul,div]
     sum_of_list = mylist[0] + mylist[1] + mylist[2] + mylist[3]
+    print("sum = ", sum )
+    print("sub  = ", sub )
+    print("mul  = ", mul )
+    print("div = ", div)
+    print("sum_of_list = ", sum_of_list)
+
+calc (3,4)
